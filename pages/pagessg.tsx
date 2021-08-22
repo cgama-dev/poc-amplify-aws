13de5d28117bd8a18095ccf5b864d3d1bf7e6a40
Cleyton Gama <cleytongama@gmail.com>
2021-08-22 10:17:02 -0300

    teste2

--- pages/pagessg.tsx
+++ pages/pagessg.tsx
@@ -1,21 +1,22 @@
-import { GetStaticProps } from "next";
+// import { GetStaticProps } from "next";
 
 export default function PageSSG( { user } ) {
     return (
-        <div>
-            <h1>{user.name}</h1>
-            <p>{user.bio}</p>
-        </div>
+        <h1>Page static</h1>
+        // <div>
+        //     <h1>{user.name}</h1>
+        //     <p>{user.bio}</p>
+        // </div>
     )
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const response = await fetch('http://api.github.com/users/cgama-dev')
-    const user = await response.json()
-    return {
-        props: {
-            user
-        },
-        revalidate: 5
-    }
-}
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//     const response = await fetch('http://api.github.com/users/cgama-dev')
+//     const user = await response.json()
+//     return {
+//         props: {
+//             user
+//         },
+//         revalidate: 5
+//     }
+// }
